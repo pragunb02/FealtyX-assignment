28bc979a9df9f13daf4514b68f86dc6b1a09d350
Pragun Bhutani <pragun.bhutani897@gmail.com>
2024-11-07 21:22:44 +0530

    url - check

--- student-api/insert_students.sh
+++ student-api/insert_students.sh
@@ -2,7 +2,7 @@
 
 for i in {1..20}
 do
-  curl -X POST http://localhost:5001/students \
+  curl -X POST https://fealtyx-1.onrender.com/ \
   -H "Content-Type: application/json" \
   -d "{\"name\": \"Student $i\", \"age\": $((18 + $i)), \"email\": \"student$i@example.com\"}"
   echo ""
--- student-api/student-frontend/src/services/api.js
+++ student-api/student-frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001';
+// const API_URL = 'http://localhost:5001';
+const API_URL='https://fealtyx-1.onrender.com/'
 
 export const fetchStudents = () => axios.get(`${API_URL}/students`);
 export const fetchStudentById = (id) => axios.get(`${API_URL}/students/${id}`);
