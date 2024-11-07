26501d08841637922d9bad9a3ac8c160e8ac66a2
Pragun Bhutani <pragun.bhutani897@gmail.com>
2024-11-08 01:55:40 +0530

    timer

--- student-api/config.py
+++ student-api/config.py
@@ -5,7 +5,7 @@ class Config:
     DEBUG = os.getenv('DEBUG', 'True') == 'True'
     OLLAMA_API_URL = os.getenv('OLLAMA_API_URL', 'http://localhost:11434/api/generate')
     OLLAMA_MODEL = os.getenv('OLLAMA_MODEL', 'llama3')
-    CACHE_DURATION = int(os.getenv('CACHE_DURATION', 300))
+    CACHE_DURATION = int(os.getenv('CACHE_DURATION', 3000))
     REQUEST_TIMEOUT = int(os.getenv('REQUEST_TIMEOUT', 30))
     MAX_RETRIES = int(os.getenv('MAX_RETRIES', 3))
     RATE_LIMIT_CALLS = int(os.getenv('RATE_LIMIT_CALLS', 100))
